f4671e1df7058cd02c4eaa3feff7d71afbe5850c
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-05-31 17:40:54 +0100

    added images to page and styled to expand

--- max-murdle/src/components/Maximal.jsx
+++ max-murdle/src/components/Maximal.jsx
@@ -1,15 +1,47 @@
 import "../styles/App.css";
+import diana from "../images/Diana.jpeg";
+import gareth from "../images/Gareth.jpg";
+import tomasz from "../images/Tomasz.jpeg";
+import yuri from "../images/Yuri.png";
+import zan from "../images/Zan.jpg";
 
 export const Maximal = () => {
   return (
-    <nav className="Nav-container">
-      <ul className="Nav-list">
-        <li>Diana</li>
-        <li>Gareth</li>
-        <li>Tomasz</li>
-        <li>Yuri</li>
-        <li>Zan</li>
-      </ul>
-    </nav>
+    <>
+      <div className="Maximal-body">
+        <div className="Maximal-container">
+          <div
+            className="panel active"
+            style={{ backgroundImage: `url(${diana})` }}
+          >
+            <h3>Diana Vozanu</h3>
+          </div>
+          <div
+            className="panel active"
+            style={{ backgroundImage: `url(${gareth})` }}
+          >
+            <h3>Gareth Williams</h3>
+          </div>
+          <div
+            className="panel active"
+            style={{ backgroundImage: `url(${tomasz})` }}
+          >
+            <h3>Tomasz Pikniczka</h3>
+          </div>
+          <div
+            className="panel active"
+            style={{ backgroundImage: `url(${yuri})` }}
+          >
+            <h3>Yuri Sorokins</h3>
+          </div>
+          <div
+            className="panel active"
+            style={{ backgroundImage: `url(${zan})` }}
+          >
+            <h3>Zan Clifton</h3>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
