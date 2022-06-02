ca35a081093ae53838f1c1dc12a72124e86ede37
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-06-02 11:23:49 +0100

    team handling profile state

--- max-murdle/src/components/Team.jsx
+++ max-murdle/src/components/Team.jsx
@@ -1,25 +1,49 @@
 import { Link } from "react-router-dom";
 
-export const Team = () => {
+export const Team = ({ setProfile }) => {
   return (
     <>
       <header className="App-header">Meet The Team</header>
       <div className="Maximal-nav">
-        <Link to="/diana">
-          <button className="Maximal-button">Diana</button>
-        </Link>
-        <Link to="/gareth">
-          <button className="Maximal-button">Gareth</button>
-        </Link>
-        <Link to="/tomasz">
-          <button className="Maximal-button">Tomasz</button>
-        </Link>
-        <Link to="/yuri">
-          <button className="Maximal-button">Yuri</button>
-        </Link>
-        <Link to="/zan">
-          <button className="Maximal-button">Zan</button>
-        </Link>
+        <button
+          className="Maximal-button"
+          value={"Diana"}
+          onClick={(event) => setProfile(event.target.value)}
+        >
+          Diana
+        </button>
+
+        <button
+          className="Maximal-button"
+          value={"Gareth"}
+          onClick={(event) => setProfile(event.target.value)}
+        >
+          Gareth
+        </button>
+
+        <button
+          className="Maximal-button"
+          value={"Tomasz"}
+          onClick={(event) => setProfile(event.target.value)}
+        >
+          Tomasz
+        </button>
+
+        <button
+          className="Maximal-button"
+          value={"Yuri"}
+          onClick={(event) => setProfile(event.target.value)}
+        >
+          Yuri
+        </button>
+
+        <button
+          className="Maximal-button"
+          value={"Zan"}
+          onClick={(event) => setProfile(event.target.value)}
+        >
+          Zan
+        </button>
       </div>
     </>
   );
